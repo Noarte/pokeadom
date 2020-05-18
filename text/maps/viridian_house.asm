379b43d18a3f08e9c3f5a3ed1eba98db8a0873b3
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-19 00:00:23 +0300

    Update viridian_house.asm

--- text/maps/viridian_house.asm
+++ text/maps/viridian_house.asm
@@ -1,16 +1,16 @@
 _ViridianHouseText1:: ; 95d71 (25:5d71)
-	text "Coming up with"
-	line "nicknames is fun,"
-	cont "but hard."
+	text "להמציא"
+	line "כינויים זה כיף,"
+	cont "אבל לא פשוט."
 
-	para "Simple names are"
-	line "the easiest to"
-	cont "remember."
+	para "שמות פשוטים"
+	line "קל יותר"
+	cont "לזכור."
 	done
 
 _ViridianHouseText2:: ; 95dc7 (25:5dc7)
-	text "My Daddy loves"
-	line "#MON too."
+	text "האבא שלי גם אוהב"
+	line "#ימונים."
 	done
 
 _ViridianHouseText_1d5b1:: ; 95de1 (25:5de1)
