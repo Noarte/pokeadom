6478a4ba58065f12bdca39aa7a81cb34f6dcac2d
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-18 23:36:15 +0300

    Update bike_shop.asm

--- text/maps/bike_shop.asm
+++ text/maps/bike_shop.asm
@@ -17,35 +17,35 @@ _BikeShopText_1d81a:: ; 98e67 (26:4e67)
 	prompt
 
 _BikeShopText_1d81f:: ; 98e83 (26:4e83)
-	text "Oh, that's..."
+	text "אה, זה..."
 
-	para "A BIKE VOUCHER!"
+	para "שובר רכישת אופניים!"
 
-	para "OK! Here you go!"
+	para "אוקי! הנה!"
 	prompt
 
 _BikeShopText_1d824:: ; 98eb2 (26:4eb2)
-	text $52, " exchanged"
-	line "the BIKE VOUCHER"
-	cont "for a BICYCLE.@@"
+	text $52, " הומר"
+	line "שובר רכישת האופניים"
+	cont "בעבור אופניים חדשות.@@"
 
 _BikeShopText_1d82a:: ; 98ee0 (26:4ee0)
-	text "Come back again"
-	line "some time!"
+	text "נשמח לראותך "
+	line "שוב בעתיד!"
 	done
 
 _BikeShopText_1d82f:: ; 98efc (26:4efc)
-	text "How do you like"
-	line "your new BICYCLE?"
+	text "אתה אוהב"
+	line "את האופניים החדשות שלך?"
 
-	para "You can take it"
+	para "אתה יכול לקחת אותם"
 	line "on CYCLING ROAD"
 	cont "and in caves!"
 	done
 
 _BikeShopText_1d834:: ; 98f4d (26:4f4d)
-	text "You better make"
-	line "room for this!"
+	text "כדאי שתפנה"
+	line "מקום עבור זה!"
 	done
 
 _BikeShopText_1d843:: ; 98f6d (26:4f6d) female
@@ -58,13 +58,13 @@ _BikeShopText_1d843:: ; 98f6d (26:4f6d) female
 	done
 
 _BikeShopText_1d85c:: ; 98fc1 (26:4fc1)
-	text "These BIKEs are"
-	line "cool, but they're"
-	cont "way expensive!"
+	text "האופניים האלו"
+	line "מגניבות, אבל הם"
+	cont "יותר מידי יקרות!"
 	done
 
 _BikeShopText_1d861:: ; 98ff2 (26:4ff2)
-	text "Wow. Your BIKE is"
-	line "really cool!"
+	text "ואו. האופניים שלך"
+	line "ממש מגניבות!"
 	done
 
